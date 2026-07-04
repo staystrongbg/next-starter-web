35d0de6741cdd240777ba91833e5c9c1b3fe6c92
ddamaja <staystrongbg@gmail.com>
2026-07-04 21:03:34 +0200

    footer font fix

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   const currentYear = new Date().getFullYear().toString();
   return (
-    <footer className="flex h-16 w-full items-center justify-center">
+    <footer className="flex h-16 w-full items-center md:text-base text-sm justify-center">
       <p className="text-gray-400">© {currentYear} Next.js Starter Kit. All rights reserved.</p>
     </footer>
   );
